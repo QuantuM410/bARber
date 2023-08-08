a1d3d6371444de9cc9510ff6b35a30a44ffaade8
satvshr <112589278+satvshr@users.noreply.github.com>
2023-08-08 21:20:09 +0530

    started wokring on register page

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-
+"use client";
+import React, { useState } from "react";
+import Image from 'next/image'
 const Register = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    email: '',
-    location: '',
-    phone: '',
-    gender: 'Male', // Default value
+    username: "",
+    password: "",
+    email: "",
+    location: "",
+    phone: "",
+    gender: "", // Default value
   });
 
   const handleInputChange = (event) => {
@@ -22,33 +23,83 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h1>Barber Shop Registration</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Username:</label>
-        <input type="text" name="username" onChange={handleInputChange} required />
-        <br />
-        <label>Password:</label>
-        <input type="password" name="password" onChange={handleInputChange} required />
-        <br />
-        <label>Email:</label>
-        <input type="email" name="email" onChange={handleInputChange} required />
-        <br />
-        <label>Location:</label>
-        <input type="text" name="location" onChange={handleInputChange} required />
-        <br />
-        <label>Phone:</label>
-        <input type="tel" name="phone" onChange={handleInputChange} required />
-        <br />
-        <label>Gender:</label>
-        <select name="gender" onChange={handleInputChange}>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Unisex">Unisex</option>
-        </select>
-        <br />
-        <button type="submit">Register</button>
-      </form>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+        <Image className="mx-auto" src="/assets/logo.png" alt="logo" width={150} height={150}/>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="username"
+              className="w-full px-3 py-2 border rounded-md"
+              placeholder="Username"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              name="password"
+              className="w-full px-3 py-2 border rounded-md"
+              placeholder="Password"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="email"
+              name="email"
+              className="w-full px-3 py-2 border rounded-md"
+              placeholder="Email"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="location"
+              className="w-full px-3 py-2 border rounded-md"
+              placeholder="Location"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="tel"
+              name="phone"
+              className="w-full px-3 py-2 border rounded-md"
+              placeholder="Phone Number"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <div className="relative">
+              <div className="absolute left-3 top-2 text-gray-400">Your clientelle</div>
+              <select
+                name="gender"
+                className="w-full px-3 py-2 border rounded-md"
+                onChange={handleInputChange}
+              >
+                <option value=""></option>
+                <option value="Unisex">Unisex</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
