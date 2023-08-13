a23dcb37f0301d72bec2fa6e5e4c5c27029ba8be
QuantuM410 <kartikeys410@gmail.com>
2023-08-13 07:56:38 +0530

    <Added>: AR effects

--- app/tryon/page.jsx
+++ app/tryon/page.jsx
@@ -1,16 +1,16 @@
-// import React from 'react'
-// import * as deepar from "deepar";
+import * as deepar from "deepar";
+import React from 'react'
 
-// const deepAR = await deepar.initialize({
-//   licenseKey: "your_license_key_here",
-//   canvas: document.getElementById("deepar-canvas"),
-//   effect: "https://cdn.jsdelivr.net/npm/deepar/effects/aviators",
-// });
+const deepAR = await deepar.initialize({
+  licenseKey: "your_license_key_here",
+  canvas: document.getElementById("deepar-canvas"),
+  effect: "https://cdn.jsdelivr.net/npm/deepar/effects/aviators",
+});
 
-// const page = () => {
-//   return (
-//     <div>page</div>
-//   )
-// }
+const page = () => {
+  return (
+    <div></div>
+  )
+}
 
-// export default page
+export default page
